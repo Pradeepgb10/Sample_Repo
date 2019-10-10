70052dec54063a888ff75430de469af1af8653da
Pradeep Balakrishna <pradeepgb10@gmail.com>
2019-10-10 14:27:40 +0200

    Updated code

--- my_first_file.py
+++ my_first_file.py
@@ -4,11 +4,17 @@ import numpy as np
 from os import rename
 import requests
 
+
 print(sys.version)
 print(sys.executable)
 
 print("********************************")
 
 def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
+    greeting = "Hello"
     return greeting
+
+res = "Function completed"
+
+print(res)
+print("End of file")
