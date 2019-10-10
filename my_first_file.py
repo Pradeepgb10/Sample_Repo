b0b3ce82918b153e680d417b95a0a6a049e09f2c
Pradeep Balakrishna <pradeepgb10@gmail.com>
2019-10-10 14:01:11 +0200

    Visual Studio code

---
+++ my_first_file.py
@@ -0,0 +1,13 @@
+import math
+import sys
+import numpy as np
+from os import rename
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
